5d963712367931f008400079aea668feb68a14dd
nikos chalikias <nikos.chalikias@gmail.com>
2020-06-29 05:57:39 +0100

    updated after review

--- generator.h
+++ generator.h
@@ -5,14 +5,15 @@
 
 class nBlock_Generator: public nBlockNode {
 public:
-    nBlock_Generator(uint32_t CLOCK uint32_t ENABLE);
-    uint32_t outputAvailable(uint32_t outputfrequency);
-    uint32_t readOutput(uint32_t outputFrequency);
-    void triggerInput(uint32_t inputFrequency);
+    nBlock_Generator(uint32_t CLOCK);
+    uint32_t outputAvailable(uint32_t outputNumber);
+    uint32_t readOutput(uint32_t outputNumber);
+    void triggerInput(uint32_t inputNumber, uint32_t value);
     void step(void);
 private:
     fifo internal_fifo;
     uint32_t input_0;
+    uint32_t input_1;
     PwmOut _pwmo;
 };
 
