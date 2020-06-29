360e9b2e080b8321eb252dc34bb071d8e133061a
nikos chalikias <nikos.chalikias@gmail.com>
2020-06-29 15:14:46 +0100

    that did not work, restored

--- generator.cpp
+++ generator.cpp
@@ -25,6 +25,7 @@ uint32_t nBlock_Generator::readOutput(uint32_t outputNumber) {  // Pass the Carr
 void nBlock_Generator::triggerInput(uint32_t inputNumber, uint32_t value) // Scan the inputs and prepare Carrot
     {   	
 	    uint32_t outFrequency;
+        int32_t divider;
     if (inputNumber == 0){                                      // "value" contains the Frequency
 	    divider = system_clock/value;
 	    outFrequency = system_clock/divider;
