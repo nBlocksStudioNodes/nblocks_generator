ba13f73978fe34b9a802cc78576960802c90e118
nikos chalikias <nikos.chalikias@gmail.com>
2020-06-29 14:55:34 +0100

    uint fixed

--- generator.cpp
+++ generator.cpp
@@ -1,7 +1,7 @@
 #include "generator.h"
 PwmOut fmclck(P2_5);                                    // for RESERVE pin21(P2_5) as PWM1[6], ATTENTION: this style brakes the nblocksStudio common practice
 // Generator
-nBlock_Generator::nBlock_Generator(int32_t THECLOCK):{  // One Parameter: THECLOCK of the LPC1768
+nBlock_Generator::nBlock_Generator(uint32_t THECLOCK):{  // One Parameter: THECLOCK of the LPC1768
 system_clock = THECLOCK;
 int32_t divider = 96000;
 LPC_PWM1->TCR = (1 << 1);               				// 1)Reset counter, disable PWM
@@ -10,7 +10,7 @@ LPC_SC->PCLKSEL0 |= (1 << 12);          				// 2)Set peripheral clock divider to
 LPC_PWM1->MR0 = divider - 1;            				// 3)Match Register 0 is shared period counter for all PWM1
 LPC_PWM1->MR6 = (divider + 1)>> 1;      				// divider=96000 for 1KHz when inputs are not connected
 LPC_PWM1->LER |= 1;                  					       
-LPC_PWM1->TCR = (1 << 0) || (1 << 3);               // 5)Enable counter and PWM 
+LPC_PWM1->TCR = (1 << 0) || (1 << 3);                   // 5)Enable counter and PWM 
     return;
 }
 uint32_t nBlock_Generator::outputAvailable(uint32_t outputNumber) { //Check for available Carrots
