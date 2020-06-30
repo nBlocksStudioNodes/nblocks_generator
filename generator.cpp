cf39f41dc695f5f20128c3349de01ba7000e95e5
nikos chalikias <nikos.chalikias@gmail.com>
2020-06-30 07:01:15 +0100

    first working version, tested without connecting inputs, Out Frequency at 1000Hz

--- generator.cpp
+++ generator.cpp
@@ -1,9 +1,9 @@
-#include "generator.h"
-PwmOut fmclck(P2_5);                                        // for RESERVE pin21(P2_5) as PWM1[6], ATTENTION: this style brakes the nblocksStudio common practice
+#include "generator.h"                                       // for RESERVE pin21(P2_5) as PWM1[6], ATTENTION: this style brakes the nblocksStudio common practice
 // Generator
 nBlock_Generator::nBlock_Generator(uint32_t THECLOCK){     // Constructor, One Parameter: THECLOCK of the LPC1768
     uint32_t system_clock;
     int32_t divider = 96000;
+PwmOut fmclck(P2_5); 
     system_clock = THECLOCK;
     LPC_PWM1->TCR = (1 << 1);               				// 1)Reset counter, disable PWM
     LPC_SC->PCLKSEL0 &= ~(0x3 << 12);  
@@ -12,6 +12,7 @@ nBlock_Generator::nBlock_Generator(uint32_t THECLOCK){     // Constructor, One P
     LPC_PWM1->MR6 = (divider + 1)>> 1;      				// divider=96000 for 1KHz when inputs are not connected
     LPC_PWM1->LER |= 1;                  					       
     LPC_PWM1->TCR = (1 << 0) || (1 << 3);                   // 5)Enable counter and PWM 
+
     return;
 }
 uint32_t nBlock_Generator::outputAvailable(uint32_t outputNumber) { //Check for available Carrots
@@ -26,6 +27,7 @@ void nBlock_Generator::triggerInput(uint32_t inputNumber, uint32_t value) // Sca
     {   	
 	uint32_t outFrequency;
     int32_t divider;
+    PwmOut fmclck(P2_5); 
     if (inputNumber == 0){                                      // "value" contains the Frequency
 	    divider = system_clock/value;
 	    outFrequency = system_clock/divider;
